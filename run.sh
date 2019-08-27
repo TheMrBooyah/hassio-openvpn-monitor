77c6e18a39181b3cb533ab6cbb211c742d6267ed
TheMrBooyah <remco_van_herk@hotmail.com>
2019-08-27 21:37:07 +0200

    Updated write path of generator configuration

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 set -e
 CONFIG_PATH="/data/options.json"
 
-mustache-cli $CONFIG_PATH /templates/server.mustache > /openvpn-monitor.conf
+mustache-cli $CONFIG_PATH /templates/server.mustache > /openvpn-monitor/openvpn-monitor.conf
 
-cat /openvpn-monitor.conf
+cat /openvpn-monitor/openvpn-monitor.conf
 
 exec "$@"
