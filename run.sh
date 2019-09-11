d19dee43ef9bc3a6807bcfa7d3cba580c6d9f175
Lars van der Ham <lars@vanderham.email>
2019-09-11 21:52:52 +0200

    Remove fluf from README Add api call for home assistant config

--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 set -e
 CONFIG_PATH="/data/options.json"
 
+curl -X GET \
+    -H "X-HASSIO-KEY: $HASSIO_TOKEN" \
+    -H "Content-Type: application/json" \
+    -o "/data/config.json" \
+    http://hassio/homeassistant/api/config
+
+cat /data/config.json
+
 mustache-cli $CONFIG_PATH /templates/server.mustache > ./openvpn-monitor.conf
 
 cat ./openvpn-monitor.conf
